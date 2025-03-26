c79a63bd23c8d5d373e3d68631400418824bf99b
ariss99nn <rodrigueznilo99@gmail.com>
2025-03-26 16:10:06 -0500

    app

---
+++ services/product_service.py
@@ -0,0 +1,8 @@
+from database import productos
+from models.productos import Producto
+def crear_producto(nombre, descripcion, precio, imagen):
+    producto = Producto(nombre, descripcion, precio, imagen)
+    productos.insert_one(producto.to_dict())
+    print("Producto creado correctamente.")
+def listar_productos():
+    return productos.find()
